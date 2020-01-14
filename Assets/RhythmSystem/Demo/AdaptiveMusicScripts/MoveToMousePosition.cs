e9a69be1a3b34a6cd8c5e2861fc61a1e98195e75
William Jackson <wjackson@mos.org>
2020-01-14 11:58:15 -0500

    AdaptiveBackgroundMusic demo scene: proximity mapping

---
+++ Assets/RhythmSystem/Demo/AdaptiveMusicScripts/MoveToMousePosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMousePosition : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float chaseDrag = .5f;
+    private Vector3 mousePos;
+    private Vector3 vel;
+    private float zPos;
+    private Vector3 targetPos;
+    private void Start()
+    {
+        targetPos = transform.position;
+        zPos = transform.position.z;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        targetPos.z = zPos;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref vel, chaseDrag, 105f);
+    }
+}
---
+++ Assets/RhythmSystem/Demo/AdaptiveMusicScripts/SetIntensityByDistance.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SetIntensityByDistance : MonoBehaviour
+{
+    public AdaptiveMusic adaptiveMusic;
+    public Transform transformA;
+    public Transform transformB;
+
+    private float cachedDistance = 0f;
+    private float minDistance = 3f;
+    
+    private void Start()
+    {
+        minDistance = Vector3.Distance(transformA.position, transformB.position);
+        cachedDistance = minDistance;
+    }
+    void Update()
+    {
+        float currentDistance = Vector3.Distance(transformA.position, transformB.position);
+        if (currentDistance != cachedDistance)
+        {
+            adaptiveMusic.SetIntensity(Mathf.InverseLerp(minDistance, 0f, currentDistance));
+            cachedDistance = currentDistance;
+        }
+    }
+}
